225b8fbc6ecaefc9726edefe082e7c39f5e193c5
not-elm <elmgameinfo@gmail.com>
2025-06-29 11:37:23 +0900

    update: add parent_searcher

--- src/system_param.rs
+++ src/system_param.rs
@@ -1,6 +1,9 @@
 mod cameras;
 mod child_searcher;
+mod parent_searcher;
 
 pub mod prelude {
-    pub use crate::system_param::{cameras::Cameras, child_searcher::ChildSearcher};
+    pub use crate::system_param::{
+        cameras::Cameras, child_searcher::ChildSearcher, parent_searcher::ParentSearcher,
+    };
 }
